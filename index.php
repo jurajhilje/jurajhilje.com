859a4349bc5460afa3b4cc4a89ea1baab3230bc1
Juraj Hilje <jhilje@gmail.com>
2016-01-22 19:03:36 +0100

    Add build system

---
+++ index.php
@@ -0,0 +1,67 @@
+<?
+$root = 'http://jurajhilje.com/';
+// $root = 'http://jurajhilje-com.dev/public/';
+$hash = '70e12eb';
+?>
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Juraj Hilje</title>
+        <meta name="description" content="JS and Swift developer">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta property="og:title" content="Juraj Hilje">
+        <meta property="og:description" content="JS and Swift developer">
+        <meta property="og:url" content="<?=$root?>">
+        <meta property="og:site_name" content="Juraj Hilje">
+        <meta property="article:author" content="Juraj Hilje">
+        <meta property="og:image" content="<?=$root?>juraj-hilje.jpg">
+        <link rel="stylesheet" href="<?=$root?>css/style.css?v=<?=$hash?>">
+        <script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');ga('create', 'UA-45983209-1', 'jurajhilje.com');ga('send', 'pageview');</script>
+    </head>
+    <body>
+        <div>
+            <span class="indent-1">
+            <em>juraj hilje: </em>
+            <br>
+            {
+            <br>
+            </span>
+                <span class="indent-2">
+                <em>job</em>: "JS and Swift developer",
+                <br>
+                <em>email</em>: "<a href="mailto:juraj.hilje@gmail.com">juraj.hilje@gmail.com</a>",
+                <br>
+                <em>skype</em>: "<a href="skype:juraj.hilje?chat">juraj.hilje</a>",
+                <br>
+                <em>linkedin</em>: "<a href="https://www.linkedin.com/in/jurajhilje" target="_blank">www.linkedin.com/in/jurajhilje</a>",
+                <br>
+                <em>twitter</em>: "<a href="https://twitter.com/juraj_hilje" target="_blank">@juraj_hilje</a>",
+                <br>
+                <em>work</em>: "<a href="http://rentl.io/" target="_blank">rentl.io</a> 12/2014-present; <a href="http://www.futuro.hr" target="_blank">www.futuro.hr</a> 09/2008-12/2014",
+                <br>
+                <em>education</em>: "<a href="http://www.tvz.hr" target="_blank">www.tvz.hr</a>, bacc.ing.techn.inf.",
+                <br>
+                <em>workflow</em>:
+                <br>
+                {
+                </span>
+                    <span class="indent-3">
+                        <em>languages</em>: "Swift, Objective-C, JavaScript, HTML, CSS, PHP, SQL",
+                        <br>
+                        <em>tools</em>: "Xcode, Atom, Git, DevTools, Sass, Grunt, Gulp, JSHint, CocoaPods, Bower, Homebrew, npm, Node.js, PM2, Yeoman, MySQL, Transmit, Tyme",
+                        <br>
+                        <em>libs</em>: "Underscore, Backbone, Polymer, jQuery, Bourbon, Neat, RequireJS, Mocha, Chai, Sinon, Blanket",
+                        <br>
+                        <em>services</em>: "Apple Dev Center, Bitbucket, GitHub, Parse, CircleCI, Braintree, Mandrill, Stack Overflow, NSScreencast, Ray Wenderlich, Tuts+, Treehouse, Deezer"
+                    </span>
+                <span class="indent-2">
+                },<br>
+                <em>apps</em>: "<a href="http://cssselector.com/" target="_blank">CSS Selector</a>"
+                </span>
+            <span class="indent-1">
+            }
+            </span>
+        </div>
+    </body>
+</html>
